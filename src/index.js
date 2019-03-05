6149eb4c0d8b0f9037a95bb99ba87518e2a519e8
Jack Steam <jacksteamdev@gmail.com>
2019-03-05 11:08:42 -0600

    Export combineArrays

--- src/index.js
+++ src/index.js
@@ -33,8 +33,8 @@ export const deriveManifest = (
   }
 }
 
-const combineArrays = (...arrays) => {
-  const flat = arrays.flat().filter(a => a)
+export const combineArrays = (...arrays) => {
+  const flat = arrays.flat(5).filter(a => a)
   const set = new Set(flat)
   const values = [...set]
 
